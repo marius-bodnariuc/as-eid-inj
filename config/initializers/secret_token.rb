d85751162043e7b6bd7daba82ff27baaa7a49090
Marius Bodnariuc <marius.bodnariuc@assist.ro>
2016-09-26 12:10:44 +0300

    Set everything up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsEidInj::Application.config.secret_token = '8ba9c72eefc18541a65788627c58dd862f119a269b72693fcd12363882f2fd73ecd73b0a37041242ad7079cfa1764817a6058327861086d7f4e63be47ba69945'
